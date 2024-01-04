c90ab12d56d7b79a93aa06760d3464967664d85c
hyeonjung <cathy02473500@gmail.com>
2024-01-04 21:50:23 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,17 +1,31 @@
+import random 
+
 num = 0
+num_computer = 0
 start_number = 1
-A = True #A의 차례인지 체크
+A = True #컴퓨터의 차례인지 체크
 
 def brGame(player1, player2):
     for i in range(num):
         global start_number
-        print(player1," : ", start_number)
+        print(player1," ", start_number)
         start_number += 1
         if start_number == 32:
             print(player2, " win!")
             break
 
 while start_number <= 31:
+    if A == True: #컴퓨터 차례
+        num_computer = random.randint(1, 3)
+        for i in range(num_computer):
+            print("computer ", start_number)
+            start_number += 1
+            if start_number == 32:
+                print("player win!")
+                break
+        A = False
+        continue
+
     while True:
         try: 
             num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
@@ -22,12 +36,7 @@ while start_number <= 31:
         except ValueError:
             print("정수를 입력하세요")
 
-    if A == True:
-        brGame("playerA", "playerB")
-        A = False
-        continue
-    
     if A == False:
-        brGame("playerB", "playerA")
+        brGame("player", "computer")
         A = True
         continue
