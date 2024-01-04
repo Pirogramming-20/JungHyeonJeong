bf628d043c0373d6feadac2f6250f1f3cf0fe0a3
hyeonjung <cathy02473500@gmail.com>
2024-01-04 20:54:37 +0900

    4단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,5 @@
 num = 0
+start_number = 1
 
 while True:
     try: 
@@ -9,3 +10,7 @@ while True:
         break
     except ValueError:
         print("정수를 입력하세요")
+
+for i in range(num):
+    print("player : ", start_number)
+    start_number += 1
