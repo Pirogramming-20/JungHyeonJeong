04a81612807421ec99200ea67b7c38f8b5fadc2e
hyeonjung <cathy02473500@gmail.com>
2024-01-04 21:15:52 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,30 +1,32 @@
 num = 0
 start_number = 1
+A = True #A의 차례인지 체크
 
-while True:
-    try: 
-        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if num not in [1, 2, 3]:
-            print("1, 2, 3 중 하나를 입력하세요")
-            continue
-        break
-    except ValueError:
-        print("정수를 입력하세요")
+while start_number <= 31:
+    while True:
+        try: 
+            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if num not in [1, 2, 3]:
+                print("1, 2, 3 중 하나를 입력하세요")
+                continue
+            break
+        except ValueError:
+            print("정수를 입력하세요")
 
-for i in range(num):
-    print("playerA : ", start_number)
-    start_number += 1
-
-while True:
-    try: 
-        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if num not in [1, 2, 3]:
-            print("1, 2, 3 중 하나를 입력하세요")
-            continue
-        break
-    except ValueError:
-        print("정수를 입력하세요")
-
-for i in range(num):
-    print("playerB : ", start_number)
-    start_number += 1
+    if A == True:
+        for i in range(num):
+            print("playerA : ", start_number)
+            start_number += 1
+            if start_number == 32:
+                break
+        A = False
+        continue
+    
+    if A == False:
+        for i in range(num):
+            print("playerB : ", start_number)
+            start_number += 1
+            if start_number == 32:
+                break
+        A = True
+        continue
